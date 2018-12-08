41b46973f2b70bd2b8dda53dd92f99a8ae042380
Augusto Rucle <augustorucle@gmail.com>
2018-12-08 02:42:23 -0600

    Se modifico la estructura para recibir un AJAX

--- resources/views/desechos.blade.php
+++ resources/views/desechos.blade.php
@@ -3,12 +3,23 @@
     <head>
         <meta charset="utf-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>Desechos</title>
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-        <script src="main.js"></script>
+        <link href="{{ URL::asset('css/app.css') }}" rel="stylesheet">
+        <link href="{{ URL::asset('css/styleDesechos.css') }}" rel="stylesheet">
+        <script src="{{ URL::asset('js/main.js') }}"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="{{ URL::asset('js/Tables.js') }}"></script>
     </head>
-    <body>
+    <body >
+        <div class="alert alert-success" id="success" role="alert">
+            Datos guardados exitosamente
+        </div>
+        <div class="alert alert-danger" id="danger" role="alert">
+            Ups, algo salio mal. Verifica tus datos!
+        </div>
         <h1>Desecho de unidades</h1>
+        <!-- Show table with Fifteen elements-->
         <div class="table-responsive">
             <table class="table table-bordered">
                 <thead>
@@ -25,129 +36,103 @@
                     </tr>
                 </thead>
                 <tbody>
-                    @foreach ($arrayData as $data)
-                        <tr>
-                            <td class="row_1">{{ $data->id }}</td>
-                            <td class="row_2">{{ $data->num_prog }}</td>
-                            <td class="row_3">{{ $data->num_unidad }}</td>
-                            <td class="row_4">{{ $data->caducidad }}</td>
-                            <td class="row_5">{{ $data->defecto_conservacion }}</td>
-                            <td class="row_6">{{ $data->num_cs }}</td>
-                            <td class="row_7">{{ $data->num_registro }}</td>
-                            <td class="row_8">{{ $data->nombre_del_que_desecha }}</td>
-                            <td class="row_9">{{ $data->nombre_personal_pbi }}</td>
-                        </tr>
-                    @endforeach
+
                 </tbody>
             </table>
         </div>
+        <nav aria-label="Page navigation example">
+            <ul class="pagination">
+                @php 
+                    $sizeArray = ceil(($size/15));
+                @endphp
+                @for($i=1; $i <= $sizeArray; $i++)
+                    @if($sizeArray >= 1)
+                        <li class="page-item"><a class="page-link" id="page_{{ $i }}" >{{ $i }}</a></li>
+                    @endif
+                @endfor
+            </ul>
+        </nav>
         <!-- Insert -->
-        <form action="{{route('insert')}}" method="POST">
             <div class="input-group mb-3">
                 <div class="input-group-prepend">
                     <span class="input-group-text" id="inputGroup-sizing-default">Numero de prog</span>
                 </div>
-                <input type="text" class="form-control" name="1" required>
+                <input type="text" class="form-control" name="num_prog" id="num_prog" required>
             </div>
             <div class="input-group mb-3">
                 <div class="input-group-prepend">
                     <span class="input-group-text" id="inputGroup-sizing-default">Numero de unidades</span>
                 </div>
-                <input type="text" class="form-control" name="2" required>
+                <input type="number" class="form-control" name="num_unidad" id="num_unidad" required>
             </div>
             <div class="input-group mb-3">
                 <div class="input-group-prepend">
                     <span class="input-group-text" id="inputGroup-sizing-default">Caducidad</span>
                 </div>
-                <input type="text" class="form-control" name="3" required>
+                <input type="date" class="form-control" name="caducidad" id="caducidad" required>
             </div>
             <div class="input-group mb-3">
                 <div class="input-group-prepend">
                     <span class="input-group-text" id="inputGroup-sizing-default">Defecto de conversión</span>
                 </div>
-                <input type="text" class="form-control" name="4" required>
+                <input type="text" class="form-control" name="defecto_conservacion" id="defecto_conservacion" required>
             </div>
             <div class="input-group mb-3">
                 <div class="input-group-prepend">
                     <span class="input-group-text" id="inputGroup-sizing-default">Numero de cs</span>
                 </div>
-                <input type="text" class="form-control" name="5" required>
+                <input type="text" class="form-control" name="num_cs" id="num_cs" required>
             </div>
             <div class="input-group mb-3">
                 <div class="input-group-prepend">
                     <span class="input-group-text" id="inputGroup-sizing-default">Numero de registro</span>
                 </div>
-                <input type="text" class="form-control" name="6" required>
+                <input type="text" class="form-control" name="num_registro" id="num_registro" required>
             </div>
             <div class="input-group mb-3">
                 <div class="input-group-prepend">
                     <span class="input-group-text" id="inputGroup-sizing-default">Nombre de quien desecha</span>
                 </div>
-                <input type="text" class="form-control" name="7" required>
+                <input type="text" class="form-control" name="nombre_desechador" id="nombre_desechador" required>
             </div>
             <div class="input-group mb-3">
                 <div class="input-group-prepend">
                     <span class="input-group-text" id="inputGroup-sizing-default">Nombre del personal PBI</span>
                 </div>
-                <input type="text" class="form-control" name="8" required>
+                <input type="text" class="form-control" name="nombre_ppbi" id="nombre_ppbi" required>
             </div>
-            @csrf
-            <input type="submit" value="Insertar">
-        </form>
+            
+            <input type="button" value="Insertar" id="buttonInsert">
+
+        <!-- Options of table -->
         <div>
             <div style="display: flex">
-                <input type="checkbox"  id="1" value="show" checked="checked" onclick="_onclick(this.id)" >
-                <p># de unidades</p>
+                <input type="checkbox"  id="1" value="show"  onclick="_onclick(this.id)" ><p># de unidades</p>
             </div>
             <div style="display: flex">
-                <input type="checkbox" id="2" value="show" checked="checked" onclick="_onclick(this.id)" ><p># de prog</p>
+                <input type="checkbox" id="2" value="show"  onclick="_onclick(this.id)" ><p># de prog</p>
             </div>
             <div style="display: flex">
-                <input type="checkbox" id="3" value="show" checked="checked" onclick="_onclick(this.id)"><p># de unidad</p>
+                <input type="checkbox" id="3" value="show"  onclick="_onclick(this.id)"><p># de unidad</p>
             </div>  
             <div style="display: flex">
-                <input type="checkbox" id="4" value="show" checked="checked" onclick="_onclick(this.id)" ><p>Caducidad</p>
+                <input type="checkbox" id="4" value="show"  onclick="_onclick(this.id)" ><p>Caducidad</p>
             </div>
             <div style="display: flex">
-                <input type="checkbox" id="5" value="show" checked="checked" onclick="_onclick(this.id)"><p>Defecto de conversión</p>
+                <input type="checkbox" id="5" value="show"  onclick="_onclick(this.id)"><p>Defecto de conversión</p>
             </div>
             <div style="display: flex">
-                <input type="checkbox" id="6" value="show" checked="checked" onclick="_onclick(this)"><p># de cs</p>
+                <input type="checkbox" id="6" value="show"  onclick="_onclick(this)"><p># de cs</p>
             </div>
             <div style="display: flex">
-                <input type="checkbox" id="7" value="show" checked="checked" onclick="_onclick(this.id)" ><p># de registro</p>
+                <input type="checkbox" id="7" value="show"  onclick="_onclick(this.id)" ><p># de registro</p>
             </div>
             <div style="display: flex">
-                <input type="checkbox" id="8" value="show" checked="checked" onclick="_onclick(this.id)" ><p>Nombre del que desecha</p>
+                <input type="checkbox" id="8" value="show"  onclick="_onclick(this.id)" ><p>Nombre del que desecha</p>
             </div>
             <div style="display: flex">
-                <input type="checkbox" id="9" value="show" checked="checked" onclick="_onclick(this.id)"><p>Nombre del personal PBI</p>
+                <input type="checkbox" id="9" value="show"  onclick="_onclick(this.id)"><p>Nombre del personal PBI</p>
             </div>
         </div>
     </body>
 </html>
-
-
-<script>
-
-    function _onclick(id){
-        let newObject =  document.getElementById(id).value;
-        var all_row=document.getElementsByClassName("row_"+id);
-        
-        if(newObject == "show"){
-
-            for(var i=0;i<all_row.length;i++){
-                all_row[i].style.display="none";
-            }
-            document.getElementById("col_"+id).style.display="none";
-            document.getElementById(id).value="hide";
-        }else{
-            for(var i=0;i<all_row.length;i++){
-                all_row[i].style.display="table-cell";
-            }
-            document.getElementById("col_"+id).style.display="table-cell";
-            document.getElementById(id).value="show";
-        }
-    }
-
-</script>
